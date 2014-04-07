eae7fd1e201a675a4d2aa52aa01580f3649f857a
AmyStephen <AmyStephen@gmail.com>
2014-04-07 10:43:09 -0500

    Molajito Changes

--- Factories/Route/RouteFactoryMethod.php
+++ Factories/Route/RouteFactoryMethod.php
@@ -44,7 +44,7 @@ class RouteFactoryMethod extends FactoryMethodBase implements FactoryInterface, 
      *
      * @return  array
      * @since   1.0
-     * @throws  \CommonApi\Exception\RuntimeException;
+     * @throws  \CommonApi\Exception\RuntimeException
      */
     public function setDependencies(array $reflection = null)
     {
@@ -81,7 +81,7 @@ class RouteFactoryMethod extends FactoryMethodBase implements FactoryInterface, 
      *
      * @return  $this
      * @since   1.0
-     * @throws  \CommonApi\Exception\RuntimeException;
+     * @throws  \CommonApi\Exception\RuntimeException
      */
     public function instantiateClass()
     {
@@ -262,7 +262,7 @@ class RouteFactoryMethod extends FactoryMethodBase implements FactoryInterface, 
      *
      * @return  array
      * @since   1.0
-     * @throws  \CommonApi\Exception\RuntimeException;
+     * @throws  \CommonApi\Exception\RuntimeException
      */
     public function getApplicationFilters()
     {
