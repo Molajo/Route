05b3fb46da80fddff9283a010fea5545d8639587
AmyStephen <AmyStephen@gmail.com>
2014-04-27 05:39:39 -0500

    Data Gathering Process

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,25 +6,17 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
-include_once __DIR__ . '/CreateClassMap.php';
-
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+$base = substr(__DIR__, 0, strlen(__DIR__) - 5);
+if (function_exists('CreateClassMap')) {
+} else {
+    include_once __DIR__ . '/CreateClassMap.php';
 }
+include_once $base . '/vendor/autoload.php';
 
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 $classmap = array();
-$classmap = createClassMap($base . '/vendor/commonapi/route', 'CommonApi\\Route\\');
-
-$results  = createClassMap($base . '/vendor/commonapi/exception', 'CommonApi\\Exception\\');
+$results  = createClassMap($base . '/Source/Adapter', 'Molajo\\Route\\Adapter\\');
 $classmap = array_merge($classmap, $results);
-
-$results  = createClassMap($base . '/Source/Handler', 'Molajo\\Render\\Adapter\\');
-$classmap = array_merge($classmap, $results);
-
-$classmap['Molajo\\Render\\Adapter'] = $base . '/Source/Adapter.php';
-ksort($classmap);
+$classmap['Molajo\\Route\\Driver'] = $base . '/Source/Driver.php';
 
 spl_autoload_register(
     function ($class) use ($classmap) {
